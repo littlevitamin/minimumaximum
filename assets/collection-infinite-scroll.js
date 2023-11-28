d2b34b4400fe95daacd6bf17fc6a963494ab66a8
vankata6 <ikkolev95@gmail.com>
2023-11-28 11:48:33 +0200

    infinite scroll to collection pages

---
+++ assets/collection-infinite-scroll.js
@@ -0,0 +1,28 @@
+var products_on_page = $('.products-on-page');
+var next_url = products_on_page.data('next-url');
+var load_more_btn = $('.load-more');
+
+function loadMoreProducts() {
+    $.ajax (
+        {
+            url: next_url,
+            type: 'GET',
+            dataType: 'html'
+        }
+    ).done(function(next_page) {
+        var new_products = $(next_page).find('.products-on-page');
+        var new_url = new_products.data('next-url');
+
+        next_url = new_url;
+
+        var productGrid = document.getElementById("main-collection-product-grid");
+        productGrid.setAttribute("data-next-url", next_url);
+
+        products_on_page.append(new_products.html());
+        load_more_btn.remove()
+
+        if(next_url === '') {
+            load_more_btn.remove()
+        }
+    })
+}
